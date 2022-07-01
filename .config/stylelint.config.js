f5d69a7e9279294e9ad15d38ca2fe9fa49c3e85e
Alex Reyes <alex@alexreyes.com>
2022-07-01 11:19:20 -0500

    u - webpack early config

---
+++ .config/stylelint.config.js
@@ -0,0 +1,52 @@
+/**
+ * Stylelint config file
+ * as configured in package.json under stylelint.extends
+ *
+ * @docs Stylelint https://stylelint.io/user-guide/
+ * @docs StylelintWebpackPlugin: https://webpack.js.org/plugins/stylelint-webpack-plugin/
+ * @docs stylelint-scss : https://github.com/kristerkari/stylelint-scss
+ * @since 1.0.0
+ */
+
+module.exports = {
+  ignoreFiles: [
+    "./htdocs/**/**/*.css",
+    "./assets/public/css/**/*.css",
+    "./vendor/**/**/*.css",
+    "./node_modules/**/**/*.css",
+    "./tests/**/**/*.css"
+  ],
+  extends: "stylelint-config-standard-scss",
+  plugins: [
+    "stylelint-scss",
+    "stylelint-order"
+  ],
+  overrides: [
+    {
+      files: ["**/*.scss"],
+    }
+  ],
+  rules: {
+    "at-rule-no-unknown": null,
+    "indentation": 2,
+    "max-line-length": 180,
+    "no-empty-source": null,
+    "scss/at-rule-no-unknown": true,
+    "declaration-colon-newline-after": "always-multi-line",
+    "selector-max-type": 4,
+    "selector-no-qualifying-type": null,
+    "time-min-milliseconds": 50,
+    "selector-max-id": 1,
+    "declaration-no-important": null,
+    "function-parentheses-newline-inside": "always-multi-line",
+    "no-descending-specificity": null,
+    "function-url-quotes": "always",
+    "order/order": [
+      [
+        "custom-properties",
+        "declarations"
+      ],
+    ],
+    "order/properties-alphabetical-order": true,
+  },
+}
---
+++ .config/webpack/watch.js
@@ -0,0 +1,38 @@
+/**
+ * This holds the configuration for Browsersync so we only reload after the
+ * other configs have completed their run.
+ *
+ * @since 1.1.0
+ */
+
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const DisableOutputWebpackPlugin = require('disable-output-webpack-plugin');
+
+module.exports = (webpackConfig) => {
+  const browserSyncOptions = {
+    files: webpackConfig.files,
+    host: webpackConfig.host,
+    port: webpackConfig.port,
+    https: webpackConfig.https,
+    proxy: webpackConfig.proxy,
+  };
+
+  console.log(browserSyncOptions);
+
+  const plugins = [
+    new DisableOutputWebpackPlugin(),
+    new BrowserSyncPlugin(browserSyncOptions, {
+      reload: webpackConfig.reload,
+    }),
+  ];
+
+  return {
+    mode: 'none',
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname),
+    },
+    plugins,
+  };
+};
